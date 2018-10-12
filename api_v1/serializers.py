8248b91ddff572f802ee05706c05ed88eda8a992
Clément François <clement.francois@epitech.eu>
2018-10-12 14:09:42 +0400

    serializers: ADD: Field for foreign key and hyperlink related fields

--- api_v1/serializers.py
+++ api_v1/serializers.py
@@ -6,24 +6,27 @@ class TodoListSerializer(serializers.ModelSerializer):
         Class used for the JSON serialization and
         SQL deserialization
     """
+    owner = serializers.ReadOnlyField(source='owner.email')
     class Meta:
         """
             Class used as a Meta class to describe model and field
             from the model
         """
         model = TodoListModel
-        fields = ('id', 'created', 'title', 'description', 'status')
+        fields = ('id', 'created', 'title', 'description', 'status', 'owner',)
 
 class UserSerializer(serializers.ModelSerializer):
     """
         Class used for the JSON serialization and
         SQL deserialization
     """
+    tasks = serializers.HyperlinkedRelatedField(many=True, view_name='todo-detail', read_only=True)
+
     class Meta:
         """
             Meta used to describe the serializer
             (fields, model, ...)
         """
         model = UserModel
-        fields = ('id', 'email', 'password', 'is_ban',)
+        fields = ('id', 'email', 'password', 'is_ban', 'tasks',)
         read_only_fields = ('email', 'password',)
