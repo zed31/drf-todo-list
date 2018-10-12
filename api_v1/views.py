74622b87eb14cac60eec046440d8001f919dbed0
Clément François <clement.francois@epitech.eu>
2018-10-12 23:04:31 +0400

    authentication: ADD: Backend and login / logout views

--- api_v1/views.py
+++ api_v1/views.py
@@ -2,9 +2,13 @@ from rest_framework import generics, permissions
 from .serializers import TodoListSerializer, UserSerializer
 from .models import TodoListModel, UserModel
 from .permissions import IsOwnerOrAdminOrReadOnly
+from .authentication import EmailBackendModel
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate, login, logout
+from rest_framework import status
 
 # Create your views here.
 class ListTodo(generics.ListCreateAPIView):
@@ -53,6 +57,46 @@ class DetailUser(generics.RetrieveUpdateDestroyAPIView):
     queryset = UserModel.objects.all()
     serializer_class = UserSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrAdminOrReadOnly,)
+
+class UserAuthenticationView(APIView):
+    """
+        Post and generate the authentication as well as login
+    """
+    def post(self, request, format=None):
+        """
+            Post method used to authenticate the user
+            check first if the user exist and if thats the case
+            we just add a new session to the cache
+
+            :param request: The request on a django request format
+            :param format: The request format
+        """
+        email = request.data.get('email', '')
+        password = request.data.get('password', '')
+        authentication_backend = EmailBackendModel()
+        user = authentication_backend.authenticate(username=email, password=password)
+        if user is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        if user.is_ban:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        login(request, user)
+        return Response(status=status.HTTP_200_OK)
+
+class LogoutView(APIView):
+    """
+        Logout the user
+    """
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, fromat=None):
+        """
+            Logout from the API
+
+            :param request: The request
+            :param format: The format of the request
+        """
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
 
 @api_view(['GET', 'POST'])
 def api_root(request, format=None):
