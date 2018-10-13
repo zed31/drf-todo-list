6b6f5068783f7c54c73e1b4c733eb98c75e09b07
Clément François <clement.francois@epitech.eu>
2018-10-13 17:53:55 +0400

    permissions: ADD: permission classes

--- api_v1/permissions.py
+++ api_v1/permissions.py
@@ -19,3 +19,39 @@ class IsOwnerOrAdminOrReadOnly(permissions.BasePermission):
         """
 
         return True if request.method in permissions.SAFE_METHODS or obj.owner == request.user or request.user.is_superuser else False
+
+class IsAdmin(permissions.BasePermission):
+    """
+        Check if the user is an administrator
+    """
+    def has_object_permission(self, request, view, obj):
+        """
+            Check if the user is a superuser. Basically a superuser
+            is an administrator inside our database
+
+            :param self: The IsAdmin class this
+            :param request: The request made by the user
+            :param view: The impacted view of the request
+            :param obj: The requested object
+
+            :return: True if the user is a superuser, False otherwise
+        """
+        return request.user.is_superuser
+
+class IsNotBanned(permissions.BasePermission):
+    """
+        Check if the user is ban and can access or not
+        to the page
+    """
+    def has_object_permission(self, request, view, obj):
+        """
+            Check if the user is not ban
+
+            :param self: The IsNotBanned class
+            :param request: The request made by the user
+            :param view: The view impacted by the request
+            :param obj: The requested object
+
+            :return: True if the user is not ban, False otherwise
+        """
+        return not request.user.is_ban
