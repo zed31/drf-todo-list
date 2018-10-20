1870cbe59dc2f572e1f514577e8f95d6136cb920
Clément François <clement.francois@epitech.eu>
2018-10-20 11:16:47 +0400

    permissions: ADD: Permissions for admin only and admin or user

--- api_v1/permissions.py
+++ api_v1/permissions.py
@@ -33,13 +33,28 @@ class IsOwnerOrAdmin(permissions.BasePermission):
             :param view: the targeted view
             :param obj: the requested object
         """
-        return obj.owner == request.user or request.user.is_superuser
+        return request.user and (obj.owner == request.user or request.user.is_superuser)
+
+class IsSameUserOrAdmin(permissions.BasePermission):
+    """Class permission used to define the access of a view"""
+
+    def has_object_permission(self, request, view, obj):
+        """
+            Reqd permission and check if the requested object
+            can be used
+
+            :param self: the self method
+            :param request: the user-made request
+            :param view: the targeted view
+            :param obj: the requested object
+        """
+        return request.user and (obj == request.user or request.user.is_superuser)
 
 class IsAdmin(permissions.BasePermission):
     """
         Check if the user is an administrator
     """
-    def has_object_permission(self, request, view, obj):
+    def has_permission(self, request, view):
         """
             Check if the user is a superuser. Basically a superuser
             is an administrator inside our database
@@ -51,7 +66,7 @@ class IsAdmin(permissions.BasePermission):
 
             :return: True if the user is a superuser, False otherwise
         """
-        return request.user.is_superuser
+        return request.user and request.user.is_superuser
 
 class IsNotBanned(permissions.BasePermission):
     """
--- api_v1/views.py
+++ api_v1/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
 from .models import TodoListModel, UserModel
-from .permissions import IsAdmin, IsNotBanned, IsOwnerOrAdminOrReadOnly, IsOwnerOrAdmin
+from .permissions import IsAdmin, IsNotBanned, IsOwnerOrAdminOrReadOnly, IsOwnerOrAdmin, IsSameUserOrAdmin
 from .serializers import TodoListSerializer, UserSerializer
 from . import urls_name
 from . import constants
@@ -46,7 +46,7 @@ class ListUser(generics.ListCreateAPIView):
     """
     queryset = UserModel.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsAdmin, IsNotBanned,)
+    permission_classes = (IsAdmin, IsNotBanned,)
 
 class DetailUser(generics.RetrieveUpdateDestroyAPIView):
     """
@@ -56,7 +56,7 @@ class DetailUser(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = UserModel.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsNotBanned,)
+    permission_classes = (IsSameUserOrAdmin, IsNotBanned,)
 
 def generate_method_information(route_name, request, format, methods):
     """
